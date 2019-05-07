94521bdec0ab2e07377efbb8f8b3dfda21d34583
lsmierzchalski <lsmierzchalski@gmail.com>
2019-05-08 00:35:34 +0200

    my part probably is finish :)

--- app.py
+++ app.py
@@ -11,43 +11,55 @@ import numpy as np
 from sympy import *
 from sympy.parsing.sympy_parser import parse_expr
 
+from interpolation import hermite
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
 
-xd = True
-
 app.layout = html.Div([
     html.H1('Interpolacja funkcji metodą Hermite’a.'),
     html.P('Projekt na WZM'),
     html.Div([
         html.Div([
-            html.H5('Funkcja:'),
+            html.H6('Funkcja'),
             dcc.Input(
                 value='sin(x)',
                 type='text',
                 id='function_input'
             ),
-            html.H5('Zakres:'),
-            html.H6('OD:'),
+            html.H6('Zakres'),
+            html.Label('od:'),
             dcc.Input(
                 value=-1.0,
                 type='number',
                 id='begin_input'
             ),
-            html.H6('DO:'),
+            html.Label('do:'),
             dcc.Input(
                 value=1.0,
                 type='number',
                 id='end_input'
             ),
-            html.H5('Typ rozkładu węzłów:'),
+            html.H6('Typ rozkładu węzłów'),
             dcc.Dropdown(
                 id='nodes_type_dropdown',
                 options=[{'label': i, 'value': i} for i in ['równoodległe', 'Czebyszewa', 'własne']],
                 value='równoodległe'
+            ),
+            html.Label('liczba wezlow(równoodległe, Czebyszewa):'),
+            dcc.Input(
+                value=10,
+                type='number',
+                id='num_nodes_input'
+            ),
+            html.Label('własne węzły:'),
+            dcc.Input(
+                value='[-1, 0, 1]',
+                type='text',
+                id='nodes_input'
             ),
             html.P(''),
             html.Button(
@@ -57,11 +69,30 @@ app.layout = html.Div([
             html.H6('Wynikowy wielomina:'),
             html.Div(
                 id='display-value',
-                children = 'Enter a value and press submit'
+                children = 'xd(x)'
+            ),
+            html.H6('Ustawienia programu:'),
+            html.Label('Początek wykresu:'),
+            dcc.Input(
+                value='-2 * pi',
+                type='text',
+                id='start_input'
+            ),
+            html.Label('Koniec wykresu:'),
+            dcc.Input(
+                value='2 * pi',
+                type='text',
+                id='stop_input'
+            ),
+            html.Label('Liczba punktów:'),
+            dcc.Input(
+                value=100,
+                type='number',
+                id='num_input'
             ),
         ], className="two columns"),
         html.Div([
-            html.H6('Wykres:'),
+            html.H6('Wykres funkcji'),
             dcc.Graph(
                 id='example-graph',
             )
@@ -69,17 +100,39 @@ app.layout = html.Div([
     ], className='row')
 ])
 
+def get_result_function(nodes, fun_str):
+    fun = parse_expr(fun_str)
+    x = Symbol('x')
+    result = hermite.hermite_interpolationg(nodes, fun, x)
+    return result
+
 @app.callback(
               dash.dependencies.Output('example-graph', 'figure'),
               [dash.dependencies.Input('start_button', 'n_clicks')],
-              [dash.dependencies.State('function_input', 'value')]
+              [dash.dependencies.State('function_input', 'value'),
+               dash.dependencies.State('start_input', 'value'),
+               dash.dependencies.State('stop_input', 'value'),
+               dash.dependencies.State('num_input', 'value'),
+               dash.dependencies.State('begin_input', 'value'),
+               dash.dependencies.State('end_input', 'value'),
+               dash.dependencies.State('num_nodes_input', 'value')
+               ]
               )
-def update_output(n_clicks, value):
+def update_graph(n_clicks, function_value, start_value, stop_value, num_value, begin_value, end_value, num_nodes_input):
     X = Symbol('x')
-    fun = parse_expr(value)
+    fun = parse_expr(function_value)
 
-    x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)
+    start = parse_expr(start_value)
+    stop = parse_expr(stop_value)
+
     f = lambdify(X, fun, 'numpy')
+
+    #TO DO nodes
+    nodes = np.linspace(float(begin_value), float(end_value), int(num_nodes_input))
+
+    steps = np.linspace(float(start), float(stop), int(num_value))
+    x = np.append(steps,nodes)
+    x.sort()
 
     data = []
     data.append(go.Scatter(
@@ -89,14 +142,45 @@ def update_output(n_clicks, value):
         name=str(fun)
     ))
 
+    fun_result = get_result_function(nodes, function_value)
+    fr = lambdify(X, fun_result, 'numpy')
+
+    data.append(go.Scatter(
+        x=x,
+        y=fr(x),
+        mode='lines',
+        name=str(fun_result)
+    ))
+
+    data.append(go.Scatter(
+        x=nodes,
+        y=fr(nodes),
+        mode='markers',
+        name='węzły'
+    ))
+
     figure = {
         'data': data,
         'layout': go.Layout(
-            height=650
+            height=800
         )
     }
 
     return figure
+
+@app.callback(
+              dash.dependencies.Output('display-value', 'children'),
+              [dash.dependencies.Input('start_button', 'n_clicks')],
+              [dash.dependencies.State('function_input', 'value'),
+               dash.dependencies.State('begin_input', 'value'),
+               dash.dependencies.State('end_input', 'value'),
+               dash.dependencies.State('num_nodes_input', 'value')]
+              )
+def update_output(n_click, function_value, begin_value, end_value, num_nodes_input):
+    # TO DO nodes
+    nodes = np.linspace(float(begin_value), float(end_value), int(num_nodes_input))
+    result = get_result_function(nodes, function_value)
+    return str(result).format()
 
 
 if __name__ == '__main__':
