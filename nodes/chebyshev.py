78c0f94ce9169c8beb6598132f5ac59f228ea93d
dominik_krystkowiak <dominik.krystkowiak@nordcloud.com>
2019-05-13 13:12:59 +0200

    Chebyshev and own nodes works

--- nodes/chebyshev.py
+++ nodes/chebyshev.py
@@ -1,13 +1,13 @@
 from math import cos, pi
 
+from numpy import array
+
 
 def create_chebyshev_nodes(a, b, n):
     nodes = []
-    for k in range(n):
+    for k in range(1, n+1):
         x_k = (a + b) / 2 + (b - a) / 2 * cos((2 * k - 1) / (2 * n) * pi)
-        # TODO remove conditional statement
-        if k == 0:
-            x_k = x_k * -1
         nodes.append(x_k)
     nodes.sort()
-    return nodes
+    return array(nodes)
+
